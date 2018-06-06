a41ba19f409c3becf2c6f060113fd7cd51ea9613
William Ravelo <drummerwilliam@gmail.com>
2018-06-06 17:57:03 -0500

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { renderRoutes  } from "./startup/routes.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(renderRoutes(), document.getElementById('root'));
 registerServiceWorker();
