997c82b8fe047f15c3a21e8b83e41f41e6b04db3
William Ravelo <drummerwilliam@gmail.com>
2018-06-15 23:40:45 -0500

    passed subscription logic to the route providr, now the subscriptions are made only once

--- src/ui/containers/RouteProvider.jsx
+++ src/ui/containers/RouteProvider.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from "reactstrap";
 import { Route, Switch } from 'react-router-dom';
+import { getReadings } from "../../api/ReadingAPI.js";
 
 import SideMenu from "../components/SideMenu.jsx";
 import HomePage from "../pages/HomePage.jsx";
@@ -8,7 +9,58 @@ import HistoryPage from "../pages/HistoryPage.jsx";
 import "../css/containers/RouteProvider.css";
 import "../css/components/Btn.css";
 
+const { ipcRenderer } = window.require('electron');
+const ObjectID = require("bson-objectid");
+
 class RouteProvider extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: []
+		};
+	}
+
+	componentDidMount() {
+		this.setupSubscriptions();
+	}
+
+	componentWillUnmount() {
+		this.unSetupSubscriptions();
+	}
+
+	// Subscriptions
+	setupSubscriptions() {
+		ipcRenderer.on("responseGetReadings", this.handleGetReadings.bind(this));
+		ipcRenderer.on("responseCreateReading", this.handleCreateReading.bind(this));
+		ipcRenderer.on("responseDeleteReading", this.handleDeleteReading.bind(this));
+	}
+
+	unSetupSubscriptions() {
+		ipcRenderer.removeListener("responseGetReadings", () => {});
+		ipcRenderer.removeListener("responseCreateReading", () => {});
+		ipcRenderer.removeListener("responseDeleteReading", () => {});
+	}
+
+	// Response Handlers
+	handleGetReadings(event, docs) {
+		const data = docs.map((d) => {
+			d._doc._id = ObjectID(d._doc._id.id).str;
+			return d._doc;	
+		});
+		this.setState({
+			data: data
+		});
+	}
+
+	handleCreateReading(event) {
+		getReadings({});
+	}
+
+	handleDeleteReading(event) {
+		getReadings({});
+	}
+
 	render() {
 		return (
 			<Container fluid={true} className="app">
@@ -18,12 +70,16 @@ class RouteProvider extends Component {
 					</Col>
 					<Col md="9" sm="9" xs="9" className="right_side">
 						<Switch>
-							<Route exact path="/" component={HomePage} />
-							<Route exact path="/history" component={HistoryPage} />
-						</Switch>
-					</Col>
-				</Row>
-			</Container>
+							<Route exact path="/" render={(props) => {
+								return React.createElement(HomePage, {...props});
+							}} />
+						<Route exact path="/history" render={(props) => {
+							return React.createElement(HistoryPage, {...props, data: this.state.data});
+						}} />
+				</Switch>
+			</Col>
+		</Row>
+	</Container>
 		);
 	}
 }
--- src/ui/pages/HistoryPage.jsx
+++ src/ui/pages/HistoryPage.jsx
@@ -5,55 +5,16 @@ import { getReadings } from "../../api/ReadingAPI.js";
 import NewHistory from "../components/NewHistory";
 import HistoryList from "../components/HistoryList";
 import "../css/pages/HistoryPage.css";
-const { ipcRenderer } = window.require('electron');
-const ObjectID = require("bson-objectid");
 
 export default class HistoryPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			data: []
 		};
 	}
 
 	componentDidMount() {
-		this.setupSubscriptions();
 		getReadings();
-	}
-
-	componentWillUnmount() {
-		this.unSetupSubscriptions();
-	}
-
-	setupSubscriptions() {
-		ipcRenderer.on("responseGetReadings", this.handleGetReadings.bind(this));
-		ipcRenderer.on("responseCreateReading", this.handleCreateReading.bind(this));
-		ipcRenderer.on("responseDeleteReading", this.handleDeleteReading.bind(this));
-	}
-
-	unSetupSubscriptions() {
-		ipcRenderer.removeListener("responseGetReadings", () => {});
-		ipcRenderer.removeListener("responseCreateReading", () => {});
-		ipcRenderer.removeListener("responseDeleteReading", () => {});
-	}
-
-	// Response handlers
-	handleGetReadings(event, docs) {
-		const data = docs.map((d) => {
-			d._doc._id = ObjectID(d._doc._id.id).str;
-			return d._doc;	
-		});
-		this.setState({
-			data: data
-		});
-	}
-
-	handleCreateReading(event) {
-		getReadings({});
-	}
-
-	handleDeleteReading(event) {
-		getReadings({});
 	}
 
 	render() {
@@ -68,7 +29,7 @@ export default class HistoryPage extends Component {
 					<Col md="4" sm="4" xs="4" className="title row_title">Options</Col>
 				</Row>
 				<NewHistory/>
-				<HistoryList data={this.state.data}/>
+				<HistoryList data={this.props.data}/>
 			</Container>
 		);
 	}
